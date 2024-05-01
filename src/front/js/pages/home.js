dd227e4746a5f21d03f4c9b614ee586f25a5a3ed
virgilio macero <virgiliomacero@gmail.com>
2024-05-01 17:37:12 -0400

    Latest modifications made from Virgilio with Component of Movie Card

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,20 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import MovieCard from "../component/MovieCard.jsx";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5 container">
+      <div className="d-grid">
+        <MovieCard
+          name="Pinocho"
+          imgUrl="https://media.poetryfoundation.org/uploads/media/default/0001/19/a8ddf0cc004e3fef6c20ae6e48cde0f10dd1f80d.jpeg"
+          movieUrl=" "
+        />
+      </div>
+    </div>
+  );
 };
