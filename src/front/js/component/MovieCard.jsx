c807e95963a8dc60b4f831672eb1c23e0d7ceabd
virgilio macero <virgiliomacero@gmail.com>
2024-05-03 11:58:17 -0400

    Latest modification to flux only with movies working

--- src/front/js/component/MovieCard.jsx
+++ src/front/js/component/MovieCard.jsx
@@ -1,63 +1,59 @@
 import React from "react";
 
 export default function MovieCard(props) {
-
   return (
-
-    <a className="text-decoration-none" href={props.movieUrl}>
-      <div
-        className="MovieCard"
-        style={{
-          background: `url(${props.imgUrl})`,
-          backgroundSize: "auto 100%",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        
-        <div className="MovieCardButtons">
-          {props.isWatched != true ? (
-            <a
-              onClick={(event) => {
-                event.preventDefault();
-                props.addToWatched(props.id);
-              }}
-            >
-              <i className="bi bi-eye-slash h3 text-dark"></i>
-            </a>
-          ) : (
-            <a
-              onClick={(event) => {
-                event.preventDefault();
-                props.removeFromWatched(props.id);
-              }}
-            >
-              <i className="bi bi-eye h3 "></i>
-            </a>
-          )}{" "}
-          {props.isFavorite != true ? (
-            <a
-              onClick={(event) => {
-                event.preventDefault();
-                props.addToFavorites(props.id);
-              }}
-            >
-              <i className="bi bi-star h3 text-dark"></i>
-            </a>
-          ) : (
-            <a
-              onClick={(event) => {
-                event.preventDefault();
-                props.addToFavorites(props.id);
-              }}
-            >
-              <i className="bi bi-star-fill h3 text-warning"></i>
-            </a>
-          )}
+    <div className={`${props.className}`}>
+      <a className="text-decoration-none" href={props.movieUrl}>
+        <div
+          className="MovieCard"
+          style={{
+            background: `url(${props.imgUrl})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="MovieCardButtons">
+            {props.isWatched != true ? (
+              <i
+                onClick={(event) => {
+                  event.preventDefault();
+                  props.addToWatched(props.id);
+                }}
+                className="bi bi-eye-slash h3 text-dark"
+              ></i>
+            ) : (
+              <i
+                onClick={(event) => {
+                  event.preventDefault();
+                  props.removeFromWatched(props.id);
+                }}
+                className="bi bi-eye h3 "
+              ></i>
+            )}{" "}
+            {props.isFavorite != true ? (
+              <i
+                onClick={(event) => {
+                  event.preventDefault();
+                  props.addToFavorites(props.id);
+                }}
+                className="bi bi-star h3 text-dark"
+              ></i>
+            ) : (
+              <i
+                onClick={(event) => {
+                  event.preventDefault();
+                  props.addToFavorites(props.id);
+                }}
+                className="bi bi-star-fill h3 text-warning"
+              ></i>
+            )}
+          </div>
+          <div className="MovieCardTitle">
+            <p>{props.name}</p>
+          </div>
         </div>
-        <div className="MovieCardTitle">
-          <p>{props.name}</p>
-        </div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 }
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-
-<button class="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-	);
+  return (
+    <button
+      className="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  );
 };
