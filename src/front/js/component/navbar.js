41e8123b3d525b76e8cf2ef283febd90ccb0b440
virgilio macero <virgiliomacero@gmail.com>
2024-05-10 16:07:05 -0400

    NavBar Finished and Styled

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,27 +1,87 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
 import WatchGoImage from "../../img/WatchGO-LightMode.png";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
   const [showNavBar, setShowNavBar] = useState(false);
+  const { store, actions } = useContext(Context);
 
   return (
-
-    <div className="position-relative d-flex justify-content-between">
-      <div>
-        <button
-          className="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2"
-          type="button"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <div>
+      {showNavBar ? (
+        <div className="Side-NavBar" id="SideNavBar">
+          <div className="d-flex flex-row-reverse">
+            <button
+              onClick={() => {
+                setShowNavBar(!showNavBar);
+              }}
+              className="btn bg-danger"
+              style={{
+                borderRadius: "0px",
+                borderBottomLeftRadius: "10px",
+                borderLeft: "2px solid black",
+                borderBottom: "2px solid black",
+              }}
+            >
+              <i className="bi bi-x-lg h4"></i>
+            </button>
+          </div>
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/favorites">
+              <li>Favorites</li>
+            </Link>
+            <Link to="/recently_watched">
+              <li>Recently Watched</li>
+            </Link>
+            <Link to="/about_us">
+              <li>About Us</li>
+            </Link>
+          </ul>
+          <ul>
+            <li>
+              <button
+                className="btn bg-danger"
+                style={{ fontWeight: "bolder", marginLeft: "10px" }}
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  actions.validatLoged();
+                  setShowNavBar(false);
+                }}
+              >
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className=" container my-4 position-relative d-flex justify-content-between">
+        <div>
+          <button
+            className="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2"
+            type="button"
+            onClick={() => {
+              setShowNavBar(!showNavBar);
+            }}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div className="position-absolute start-50 translate-middle-x">
+          <Link to="/">
+            <img
+              src={WatchGoImage}
+              alt="Watch & Go"
+              style={{ width: "200px" }}
+            />
+          </Link>
+        </div>
       </div>
-      <div className="position-absolute start-50 translate-middle-x">
-        <Link to="/">
-          <img src={WatchGoImage} alt="Watch & Go" style={{ width: "160px" }} />
-        </Link>
-      </div>
-
     </div>
   );
 };
