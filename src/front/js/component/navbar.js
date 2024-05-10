c0124a096883e1d45195471b5075f3b63808b139
virgilio macero <virgiliomacero@gmail.com>
2024-05-10 14:47:16 -0400

    simple mod to NavBar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [showNavBar, setShowNavBar] = useState(false);
+
   return (
-    <button
-      className="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <div>
+      <button
+        className="navbar-toggler navbar-light text-dark border border-secondary mx-2 my-2"
+        type="button"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {}
+    </div>
   );
 };
