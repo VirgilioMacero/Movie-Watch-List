1b786134988f0bcc4608c87cb3258197ba22d51a
Sony Raymond <58698452+Wilclerson@users.noreply.github.com>
2024-05-13 15:15:19 +0000

    About Us View Added with Images and PDF's

--- src/front/js/pages/about.js
+++ src/front/js/pages/about.js
@@ -1,72 +1,112 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Context } from "../store/appContext";
-import FilmCard from "../component/FilmCard.jsx";
-import { Search } from "../component/search";
-import { Toggle } from "../component/toggle.js";
+import React, { useState } from "react";
+import { Card, Button, Modal } from "react-bootstrap";
+import VirgiliosResume from "../../../../public/VirgiliosResume.pdf";
+import SonysResume from "../../../../public/SonysResume.pdf";
+import VirgilioImage from "../../../../public/virgilio-img.png";
+import SonyImage from "../../../../public/sony-img.png";
 
 export const About = () => {
-  const { store, actions } = useContext(Context);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState(""); // Track search query
+  const [selectedResume, setSelectedResume] = useState(null);
+  const [selectedName, setSelectedName] = useState(null);
 
-  useEffect(() => {
-    if (store.isSeriesActive) {
-      actions.getSeriesByName("A").then(() => {
-        setIsLoading(false);
-      });
-    } else {
-      actions.getMoviesByName("A").then(() => {
-        setIsLoading(false);
-      });
+  const handleResumeClick = (resume, name) => {
+    setSelectedResume(resume);
+    setSelectedName(name);
+    if (window.innerWidth <= 768) {
+      window.location.href = resume; // Redirect to the selected resume page
     }
-  }, [store.isSeriesActive]); // Fetch data when isSeriesActive changes
+  };
 
-  useEffect(() => {
-    if (searchQuery === "") {
-      // If searchQuery is empty, reload initial movies or series
-      if (store.isSeriesActive) {
-        actions.getSeriesByName("A");
-      } else {
-        actions.getMoviesByName("A");
-      }
-    } else {
-      // If searchQuery is not empty, search for movies or series
-      if (store.isSeriesActive) {
-        actions.getSeriesByName(searchQuery);
-      } else {
-        actions.getMoviesByName(searchQuery);
-      }
-    }
-  }, [searchQuery, store.isSeriesActive]); // Reload when searchQuery or isSeriesActive changes
+  const handleModalOpen = (resume, name) => {
+    setSelectedResume(resume);
+    setSelectedName(name);
+  };
+
+  const handleModalClose = () => {
+    setSelectedResume(null);
+    setSelectedName(null);
+  };
 
   return (
     <div className="text-center mt-5 container">
-        <h2> About Us </h2>
-      <Toggle />
-      <Search setSearchQuery={setSearchQuery} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="row">
-          {store.films.map((film) => {
-            if (film.backdrop_path != null) {
-              return (
-                <FilmCard
-                  key={film.id}
-                  name={
-                    film.original_title
-                      ? film.original_title.substring(0, 35)
-                      : film.name.substring(0, 35)
-                  }
-                  imgUrl={`https://image.tmdb.org/t/p/original${film.backdrop_path}`}
-                  filmUrl={`/single/${film.id}`}
-                  className="col mt-3"
-                />
-              );
-            }
-          })}
+      <h2>About Us</h2>
+      <div className="row mt-4">
+        {/* First Card */}
+        <div className="col-md-6">
+          <Card>
+            <Card.Img variant="top" src={VirgilioImage} alt="Virgilio Macero" style={{ height: "400px" }} />
+            <Card.Body>
+            <Card.Title style={{ fontSize: '28px' }}>Virgilio Macero</Card.Title>
+              <div className="d-flex justify-content-around">
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" href="https://www.linkedin.com/in/virgilio-macero/">
+                    <i className="fab fa-linkedin fa-2x"></i>
+                  </Button>
+                  <span>LinkedIn</span>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" onClick={() => handleResumeClick(VirgiliosResume, "Virgilio Macero")}>
+                    <i className="far fa-file-alt fa-2x"></i>
+                  </Button>
+                  <span>Resume</span>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" href="https://github.com/VirgilioMacero">
+                    <i className="fab fa-github fa-2x"></i>
+                  </Button>
+                  <span>Github</span>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
-      )}
+        {/* Second Card */}
+        <div className="col-md-6">
+          <Card>
+            <Card.Img variant="top" src={SonyImage} alt="Sony Raymond" style={{ height: "400px", width: "" }} />
+            <Card.Body>
+            <Card.Title style={{ fontSize: '28px' }}>Sony Raymond </Card.Title>
+              <div className="d-flex justify-content-around">
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" href="https://www.linkedin.com/in/wilclerson/">
+                    <i className="fab fa-linkedin fa-2x"></i>
+                  </Button>
+                  <span>LinkedIn</span>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" onClick={() => handleResumeClick(SonysResume, "Sony Raymond")}>
+                    <i className="far fa-file-alt fa-2x"></i>
+                  </Button>
+                  <span>Resume</span>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                  <Button variant="link" href="https://github.com/Wilclerson/">
+                    <i className="fab fa-github fa-2x"></i>
+                  </Button>
+                  <span>Github</span>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+      {/* Resume Modal */}
+      <Modal show={selectedResume !== null} onHide={handleModalClose} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedName}'s Resume</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <embed src={selectedResume} type="application/pdf" width="100%" height="650px" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Close
+          </Button>
+          <Button variant="primary" href={selectedResume} download>
+            Download
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
